e1248fdfe8785e02cb8474a79f5a417182eee157
thenPie <118397975+thenPie@users.noreply.github.com>
2022-12-25 21:48:39 +0300

    did zadacha64

---
+++ zadacha64/Program.cs
@@ -0,0 +1,17 @@
+﻿void AskToDescend() {
+    Console.Write("Введите число: ");
+    string input = Console.ReadLine();
+    if(int.TryParse(input, out int value)) {
+        DescendShow(value);
+    }
+}
+
+void DescendShow(int num) {
+    if(0 < num) {
+        Console.Write($"{num} ");
+        num--;
+        DescendShow(num);
+    }
+}
+
+AskToDescend();
