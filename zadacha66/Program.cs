4d48c2317f5697df2489d19d446e1a458fe0e53a
thenPie <118397975+thenPie@users.noreply.github.com>
2022-12-25 21:47:10 +0300

    did zadacha66

---
+++ zadacha66/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите два числа через пробел: ");
+var input = Console.ReadLine();
+var space = input.Split(" ");
+int m = int.Parse(space[0]);
+int n = int.Parse(space[1]);
+
+int sum = 0;
+
+if(m <= n) {
+    while(m <= n) {
+        sum = sum + m;
+        m++;
+    }
+} else if(n <= m) {
+    while(n <= m) {
+        sum = sum + n;
+        n++;
+    }
+}
+
+Console.Write(sum);
