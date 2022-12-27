ed88a187ec19db76cd3e551312c33ed7d2467187
thenPie <118397975+thenPie@users.noreply.github.com>
2022-12-27 10:15:47 +0300

    did zadacha68

---
+++ zadacha68/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите два числа через пробел: ");
+var input = Console.ReadLine();
+var space = input.Split(" ");
+int m = int.Parse(space[0]);
+int n = int.Parse(space[1]);
+
+int ack(int m, int n) {
+    if (m == 0) {
+        return n + 1;
+    }
+    else if ((m > 0) && (n == 0)) {
+        return ack(m - 1, 1);
+    }
+    else if ((m > 0) && (n > 0)) {
+        return ack(m - 1, ack(m, n - 1));
+    } else
+        return n + 1;
+}
+
+if(0 < m && 0 < n) {
+    Console.Write(ack(m, n));
+} else {
+    Console.Write("No");
+}
